524eca2a7c39debe633ceac5af99ff5903163a2a
RodrigoLopezTorres <111547293+RodrigoLopezTorres@users.noreply.github.com>
2022-08-28 17:23:15 -0500

    Update sketch.js
    Actualizado hasta el punto8 de la practica3

--- sketch.js
+++ sketch.js
@@ -1,22 +1,35 @@
 let qt;
 let count = 0;
 
-function setup() {
-   createCanvas(400 ,400);
+function setup(){
+  createCanvas(400, 400);
+  // punto central y medio del ancho y la altura
+  let boundary = new Rectangle(200, 200, 200, 200);
 
-   // centre point and half of width and height
-   let boundary = new Rectangle(200, 200, 200, 200);
+  // cada hoja solo puede tener 4 elementos
+  qt = new QuadTree(boundary, 4);
 
-   // each leave just could have 4 elements
-   qt = new QuadTree(boundary, 4);
+  console.log(qt);
+  for (let i=0; i < 25; i++){
+    let p = new Point(Math.random() * 400, Math.random() * 400);
+    qt.insert(p);
+  }
+  background(0);
+  qt.show();
+}
 
-   console.log(qt);
+function draw(){
+  background(0);
+  qt.show();
+  stroke(0, 255, 0);
+  rectMode(CENTER);
+  let range = new Rectangle(mouseX, mouseY, 50, 50)
+  rect(range.x, range.y, range.w*2, range.h*2);
+  let points = [];
+  qt.query(range , points);
 
-   for (let i=0; i < 3; i ++) {
-      let p = new Point(Math.random()*400, Math.random()*400);
-      qt.insert(p);
-   }
-
-   background(0);
-   qt.show();
+  for (let p of points){
+    strokeWeight(4);
+    point(p.x, p.y);
+  }
 }
