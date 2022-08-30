f5ebea621956c3199b318c09f82ca92661198562
RodrigoLopezTorres <111547293+RodrigoLopezTorres@users.noreply.github.com>
2022-08-30 09:45:45 -0500

    Update quadtree.js
    Buenas tardes compañeros, modifiqué un poco el contains de la clase QuadTree y agregué lo que faltaba de la practica3. Denle una chequeada si se tiene que cambiar algo. Saludos

--- quadtree.js
+++ quadtree.js
@@ -1,93 +1,137 @@
 class Point {
-   constructor (x, y, userData ){
+    constructor(x, y, userData) {
       this.x = x;
       this.y = y;
       this.userData = userData;
-   }
-}
-
-class Rectangle {
-   constructor (x, y, w, h) {
-      this.x = x; // center
+    }
+  }
+  
+  class Rectangle {
+    constructor(x, y, w, h) {
+      this.x = x; //center
       this.y = y;
-      this.w = w; // half width
-      this.h = h; // half height
-   }
+      this.w = w; //half width
+      this.h = h; //half height
+    }
 
-   // verifica si este objeto contiene un objeto Punto
-   contains(point) {
-      if (point.x >= (this.x - this.w) && point.x <= (this.w*2) && point.y >= (this.y - this.h) && point.y <= (this.h*2))
-         return true;
-      return false;
-   }
-
-   // verifica si este objeto se intersecta con otro objeto Rectangle
-   intersects(range) {
-      var punto1 = new Point(range.x - range.w, range.y - range.h);
-      var punto2 = new Point(range.x - range.w, range.y + range.h);
-      var punto3 = new Point(range.x + range.w, range.y - range.h);
-      var punto4 = new Point(range.x + range.w, range.y + range.h);
-      if (this.contains(punto1)) return true;
-      if (this.contains(punto2)) return true;
-      if (this.contains(punto3)) return true;
-      if (this.contains(punto4)) return true;
-      return false;
-   }
-}
-
-class QuadTree {
-   constructor(boundary , n) {
-      this.boundary = boundary; // Rectangle
+    //TRABAJO
+    // verificia si este objeto contiene un objeto Punto
+    contains(point) {
+        if (point.x >= (this.x - this.w) && point.x < (this.x + this.w) && point.y >= (this.y - this.h) && point.y < (this.y + this.h))
+            return true;
+        return false;
+    }
+  
+    //TRABAJO
+    // verificia si este objeto se intersecta con otro objeto Rectangle
+    intersects(range) {
+        var punto1 = new Point(range.x - range.w, range.y - range.h);
+        var punto2 = new Point(range.x - range.w, range.y + range.h);
+        var punto3 = new Point(range.x + range.w, range.y - range.h);
+        var punto4 = new Point(range.x + range.w, range.y + range.h);
+        if (this.contains(punto1)) return true;
+        if (this.contains(punto2)) return true;
+        if (this.contains(punto3)) return true;
+        if (this.contains(punto4)) return true;
+        return false;
+    }
+  }
+  
+  class QuadTree {
+    constructor(boundary, n) {
+      this.boundary = boundary; //Rectangle
       this.capacity = n; // capacidad maxima de cada cuadrante
-      this.points = []; // vector , almacena los puntos a almacenar
+      this.points = []; // vector donde almacena los puntos
       this.divided = false;
-   }
-   
-   // divide el quadtree en 4 quadtrees
-   subdivide() {
-      // Algoritmo
-      // 1: Crear 4 hijos: qt_northeast , qt_northwest , qt_southeast , qt_southwest
-      // 2: Asignar los QuadTree creados a cada hijo
-      // this.northeast = qt_northeast;
-      // this.northwest = qt_northwest;
-      // this.southeast = qt_southeast;
-      // this.southwest = qt_southwest;
-      // 3.- Hacer: this.divided <- true
-   }
-   
-   insert(point) {
-      // Algoritmo
-      // 1: Si el punto no esta en los limites ( boundary ) del quadtree Return
-      // 2: Si ( this.points.length ) < ( this.capacity ),
-      // 2.1 Insertamos en el vector this.points
-      // Sino
-      // 2.2 Dividimos si aun no ha sido dividido
-      // 2.3 Insertamos recursivamente en los 4 hijos.
-      // this.northeast.insert ( point );
-      // this.northwest.insert ( point );
-      // this.southeast.insert ( point );
-      // this.southwest.insert ( point );
-   }
-   
-   show() {
+    }
+  
+    //TRABAJO
+    // divide el quadtree en 4 quadtrees
+    subdivide() {
+        let x = this.boundary.x;
+        let y = this.boundary.y;
+        let w = this.boundary.w;
+        let h = this.boundary.h;
+
+        let qt_northeast = new Rectangle(x + w / 2, y + h / 2, w / 2, h / 2);
+        let qt_northwest = new Rectangle(x - w / 2, y + h / 2, w / 2, h / 2);
+        let qt_southeast = new Rectangle(x + w / 2, y - h / 2, w / 2, h / 2);
+        let qt_southwest = new Rectangle(x - w / 2, y - h / 2, w / 2, h / 2);
+
+        this.northeast = new QuadTree(qt_northeast, this.capacity);
+        this.northwest = new QuadTree(qt_northwest, this.capacity);
+        this.southeast = new QuadTree(qt_southeast, this.capacity);
+        this.southwest = new QuadTree(qt_southwest, this.capacity);
+
+        this.divided = true;
+    }
+  
+    //TRABAJO
+    insert(point) {
+      if (!this.boundary.contains(point)) {
+        return false;
+      }
+  
+      if (this.points.length < this.capacity) {
+        this.points.push(point);
+        return true;
+      } else {
+        if (!this.divided) {
+          this.subdivide();
+        }
+        if (this.northeast.insert(point)) {
+          return true;
+        } else if (this.northwest.insert(point)) {
+          return true;
+        } else if (this.southeast.insert(point)) {
+          return true;
+        } else if (this.southwest.insert(point)) {
+          return true;
+        }
+      }
+    }
+  
+    //TRABAJO
+    query(range, found) {
+      if (!found) {
+        found = [];
+      }
+      if (!this.boundary.intersects(range)) {
+        return;
+      } else {
+        for (let p of this.points) {
+          if (range.contains(p)) {
+            found.push(p);
+          }
+        }
+        if (this.divided) {
+          this.northwest.query(range, found);
+          this.northeast.query(range, found);
+          this.southwest.query(range, found);
+          this.southeast.query(range, found);
+        }
+      }
+      return found;
+    }
+  
+  
+    show() {
       stroke(255);
       strokeWeight(1);
       noFill();
       rectMode(CENTER);
-      rect(this.boundary.x, this.boundary.y, this.boundary.w*2, this.boundary.h*2) ;
+      rect(this.boundary.x, this.boundary.y, this.boundary.w*2, this.boundary.h*2);
+  
       if (this.divided) {
-         this.northeast.show();
-         this.northwest.show();
-         this.southeast.show();
-         this.southwest.show();
-      }
-      for (let p of this.points ) {
-         strokeWeight (4) ;
-         point (p.x , p.y );
-      }
-   }
+        this.northeast.show();
+        this.northwest.show();
+        this.southeast.show();
+        this.southwest.show();
+      }   
 
-   query(range, found) {
-
-   }
+      for (let p of this.points) {
+        strokeWeight(4);
+        point(p.x, p.y);
+      }
+  }
 }
